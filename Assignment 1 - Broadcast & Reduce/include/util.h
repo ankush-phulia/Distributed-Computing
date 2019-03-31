7738a48bd2b60e53339b83ee4bf1b3e36eb0fa5b
ankush-phulia <ankush.phulia22@gmail.com>
2019-03-31 21:14:14 +0530

    Update utils

--- Assignment 1 - Broadcast & Reduce/include/util.h
+++ Assignment 1 - Broadcast & Reduce/include/util.h
@@ -3,4 +3,24 @@
 
 #include <mpi.h>
 
+inline int getGlobalRank() {
+    int globalRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &globalRank);
+    return globalRank;
+}
+
+inline int getLocalRank(MPI_Comm comm) {
+    int localRank;
+    MPI_Comm_rank(comm, &localRank);
+    return localRank;
+}
+
+inline int getNumProcs(MPI_Comm comm) {
+    int numProcs;
+    MPI_Comm_size(comm, &numProcs);
+    return numProcs;
+}
+
+MPI_Comm createSquareMesh(MPI_Comm comm);
+
 #endif
---
+++ Assignment 1 - Broadcast & Reduce/src/util.cpp
@@ -0,0 +1,14 @@
+#include "util.h"
+
+#include <math.h> 
+
+MPI_Comm createSquareMesh(MPI_Comm comm) {
+    int numProcs = getNumProcs(comm);
+    int dims[2] = {(int) sqrt(numProcs), (int) sqrt(numProcs)};
+    int periodic[2] = {0, 0};
+    
+    // create a square mesh topology with all the processes
+    MPI_Comm mesh;
+    MPI_Cart_create(comm, 2, dims, periodic, 1, &mesh);
+    return mesh;
+}
